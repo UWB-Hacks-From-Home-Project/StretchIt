fee140f28575dff646bcf382e60b14c870ea404a
big206chris <big206chris@gmail.com>
2021-04-10 12:46:36 -0700

    updated settings

--- js/options.js
+++ js/options.js
@@ -2,17 +2,20 @@ var Settings = {
     suggestStretch: $('#suggestStretch'),
     enableSounds: $('#sounds'),
     enablePopup: $('#popup'),
+    countKeyStrokes: $('#canCountKeys'),
 
     // creates settings if first time
     init: function(){
         localStorage.setItem('suggestStretch', true);
         localStorage.setItem('sounds', true);
         localStorage.setItem('popup', true);
+        localStorage.setItem('canCountKeys', true);
         //localStorage.setItem('name', val);
 
         this.suggestStretch.val(true)
         this.enableSounds.val(true)
         this.enablePopup.val(true)
+        this.countKeyStrokes.val(true)
     },
 
     //gets saved settings
@@ -20,7 +23,8 @@ var Settings = {
         var allOptions = {
             suggestStretch: localStorage.suggestStretch,
             enableSounds: localStorage.sounds,
-            enablePopup: localStorage.popup
+            enablePopup: localStorage.popup,
+            countKeyStrokes: localStorage.canCountKeys
         }
         return allOptions
     },
@@ -37,7 +41,7 @@ var Settings = {
         localStorage.suggestStretch.val(this.suggestStretch.value)
         localStorage.sounds.val(this.sounds.value)
         localStorage.popup.val(this.popup.value)
-
+        localStorage.canCountKeys.bal(this.countKeyStrokes.value)
         update()
     },
 
@@ -46,5 +50,4 @@ var Settings = {
 //pops up a little notification that the options are saved
 function update(){
     //idk how to send a noto yet
-    print(updated)
 }
