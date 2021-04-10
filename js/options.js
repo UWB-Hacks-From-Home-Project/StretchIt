5513c8358f041b30b0762a4acabee62e2c33bb89
big206chris <big206chris@gmail.com>
2021-04-09 23:12:54 -0700

    bones for options

--- js/options.js
+++ js/options.js
@@ -2,11 +2,28 @@ var Settings = {
     suggestStretch: $('#default'),
     enableSounds: $('#sound'),
 
+    // creates settings if first time
     init: function(){
+        /* 
         localStorage.setItem('suggestStretch', true);
         localStorage.setItem('Sounds', true);
-        //localStorage.setItem('');
+        localStorage.setItem('');
+        */
+    },
 
-        this
-    }
+    //gets saved settings
+    getOptions: function(){
+
+    },
+
+    //saves current settings
+    save: function(){
+
+    },
+
 };
+
+//pops up a little notification that the options are saved
+function update(){
+    
+}
