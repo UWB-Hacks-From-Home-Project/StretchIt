66ec9f5da0aa5b7ff4e420488b72e102c73ce700
vvv317 <vincentxiao697@gmail.com>
2021-04-10 23:17:07 -0700

    added html and js for timers

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,25 @@
-var button = document.getElementById("notif");
-button.addEventListener('click', () => {
-    chrome.storage.local.get(['breakfreq'], (res) => {
-        chrome.runtime.sendMessage('', {
-        type: 'notification',
-        options: {
-            title: 'ayo',
-            message: 'wassup, break frequency is ' + res['breakfreq'],
-            contextMessage: 'yeah...',
-            iconUrl: 'logo128.png',
-            type: 'basic',
-            buttons: [{
-                title: "open google?",
-            }, {
-                title: "sorry!",
-            }]
+//break alarm
+var timing = setInterval(
+    function(){
+        chrome.alarms.get("breakAlarm", (alarm) => {
+            var yourDateToGo = alarm.scheduledTime;
+
+            var currentDate = new Date().getTime(); 
+            var timeLeft = yourDateToGo - currentDate;
+
+            var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            if (hours < 10) hours="0"+hours;
+            var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+            if (minutes < 10) minutes="0"+minutes;
+            var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+            if (seconds < 10) seconds="0"+seconds;
+
+            document.getElementById("breakReminderTimer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+
+            if (timeLeft <= 0) {
+            clearInterval(timing);
+            document.getElementById("countdown").innerHTML = "It's over";
             }
         });
-    });
-});
+    }, 1000
+);
