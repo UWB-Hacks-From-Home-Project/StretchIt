5ca202ac14300b1f2a866709ebc9b739ccd4bf51
vvv317 <vincentxiao697@gmail.com>
2021-04-10 18:19:19 -0700

    added timed notifications for posture/break + dynamic posture break length

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.create({
     url: "welcome.html"
@@ -5,29 +7,76 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  chrome.notifications.create('', {
-    title: 'ayo',
-    message: 'wassup',
-    contextMessage: 'yeaaa',
-    iconUrl: 'logo128.png',
-    type: 'basic'
-  });
-});
+    if(alarm.name === "breakAlarm")
+    {
+        var notificationID = "breakNotif";
+        var notifMessage = {
+            title: 'Take a break!',
+            message: 'It\'s been too long since your last break!',
+            contextMessage: 'From StretchIt',
+            iconUrl: 'logo128.png',
+            type: 'basic'
+        };
+        chrome.notifications.create(notificationID, notifMessage);
+    }
 
+    if(alarm.name === "postureAlarm")
+    {
+        var notificationID = "postureNotif";
+        var notifMessage = {
+            title: 'Fix your posture!',
+            message: 'It\'s a good time to do some posture exercises now. Have you had good posture?',
+            contextMessage: 'From StretchIt',
+            iconUrl: 'logo128.png',
+            type: 'basic',
+            buttons: [{
+                title: "Yes",
+            }, {
+                title: "No",
+            }]
+        };
+        chrome.notifications.create(notificationID, notifMessage);
+    }
+});
 
 chrome.runtime.onMessage.addListener(data => {
     if (data.type === 'notification') {
-      chrome.notifications.create('', data.options), (id) => {testNotificationID = id;};
+      chrome.notifications.create('testNotif', data.options)
     }
   });
 
 chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
-    if(buttonIndex === 0)
+    if(notificationId === "testNotif")
     {
-        window.open("https://www.google.com");
+        if(buttonIndex === 0)
+        {
+            window.open("https://www.google.com");
+        }
+        else if(buttonIndex === 1)
+        {
+            alert("sorry!");
+        }
     }
-    else if(buttonIndex === 1)
+
+    if(notificationId === "postureNotif")
     {
-        alert("sorry!");
+        if(buttonIndex === 0)
+        {
+            chrome.storage.local.get(['posturefreq'], (res) => {
+                chrome.local.storage.set(
+                    {posturefreq: (parseInt(res['posturefreq']) + parseInt(1))}, 
+                    () => alert((parseInt(res['posturefreq']) + parseInt(1)))
+                )
+            });
+        }
+        else if(buttonIndex === 1)
+        {
+            chrome.storage.local.get(['posturefreq'], (res) => {
+                chrome.local.storage.set(
+                    {posturefreq: (parseInt(res['posturefreq']) - parseInt(1))}, 
+                    () => alert((parseInt(res['posturefreq']) - parseInt(1)))
+                )
+            });
+        }
     }
 });
--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,20 @@
 var button = document.getElementById("notif");
 button.addEventListener('click', () => {
-    chrome.runtime.sendMessage('', {
-      type: 'notification',
-      options: {
-        title: 'ayo',
-        message: 'wassup',
-        contextMessage: 'yeah...',
-        iconUrl: 'logo128.png',
-        type: 'basic',
-        buttons: [{
-            title: "google?",
-        }, {
-            title: "sorry!",
-        }]
-        }
+    chrome.storage.local.get(['breakfreq'], (res) => {
+        chrome.runtime.sendMessage('', {
+        type: 'notification',
+        options: {
+            title: 'ayo',
+            message: 'wassup, break frequency is ' + res['breakfreq'],
+            contextMessage: 'yeah...',
+            iconUrl: 'logo128.png',
+            type: 'basic',
+            buttons: [{
+                title: "open google?",
+            }, {
+                title: "sorry!",
+            }]
+            }
+        });
     });
 });
