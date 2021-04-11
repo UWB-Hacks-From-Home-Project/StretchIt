70f8283d0b1dc43db2ede1cea06f66124eb8e201
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2021-04-11 02:06:48 -0700

    Fix final bugs

--- js/content.js
+++ js/content.js
@@ -12,6 +12,7 @@ let keyCount = 0;
 document.onkeypress = () => {
     if (countKey) {
         keyCount++;
+        console.log(keyCount)
         if (keyCount > 10) {
             chrome.runtime.sendMessage({
                 type: "typing",
